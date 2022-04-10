60825f578057539d26d2dc04634ec065f8b968c9
Le Kiet <lekietvn@gmail.com>
2022-04-10 16:10:56 +0700

    style web page & create dynamic links of Products, Customer

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views # from base project - crm1
 # đây là nơi xử lý vụ: user nhập url, file này có nhiệm vụ gửi tín hiệu tới các views để show ra giao diện
 
 urlpatterns = [
-    path('', views.home), # [base page, view connect to the base page]
-    path('products/', views.products),
-    path('customer/', views.customer),
+    path('', views.home, name="home"), # [base page, view connect to the base page]
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk>/', views.customer, name="customer"), # this url is dynamic
 ]
