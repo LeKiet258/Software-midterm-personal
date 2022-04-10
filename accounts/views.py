542fbbb44928a88a4a683588faabc19913d5d622
Le Kiet <lekietvn@gmail.com>
2022-04-10 15:25:49 +0700

    Rendering Data to Templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,27 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # home page
 def home(request):
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers,
+               'total_customers': total_customers, 'total_orders': total_orders,
+               'delivered': delivered, 'pending': pending}
+    
     # tìm tới nơi mô tả cách 'Dashboard' (nơi này còn gọi là 'template') dc hiển thị lên màn hình & render nó
-    return render(request, 'accounts/dashboard.html')  # render() làm cho chữ đẹp hơn (bằng việc tuân theo format htm)
+    return render(request, 'accounts/dashboard.html', context)  # render() làm cho chữ đẹp hơn (bằng việc tuân theo format htm)
 
 def products(request):
-    return render(request, 'accounts/products.html') 
+    products = Product.objects.all() # query the db for all products
+    return render(request, 'accounts/products.html', {'products': products}) # pass biến 'products' vào template html để dùng
 
 # customer page
 def customer(request):
