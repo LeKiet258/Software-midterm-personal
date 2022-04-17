e7cf7fbf8e60ecbcdf4e106e66352339316b0406
Le Kiet <lekietvn@gmail.com>
2022-04-17 22:23:47 +0700

    add forget password functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
+# đây là nơi xử lý vụ: user nhập url, file này có nhiệm vụ gửi tín hiệu tới các views để show ra giao diện
 from django.urls import path
 from . import views # from base project - crm1
-# đây là nơi xử lý vụ: user nhập url, file này có nhiệm vụ gửi tín hiệu tới các views để show ra giao diện
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register_page, name="register"),
@@ -15,5 +16,26 @@ urlpatterns = [
     
     path('create_order/<str:pk>', views.create_order, name='create_order'),
     path('update_order/<str:pk>', views.update_order, name='update_order'),
-    path('delete_order/<str:pk>', views.delete_order, name='delete_order')
+    path('delete_order/<str:pk>', views.delete_order, name='delete_order'),
+    
+    # build reset password urls
+    path('reset_password/',
+        auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+        name="reset_password"),
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+        name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+        name="password_reset_confirm"),
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+        name="password_reset_complete"),
 ]
+
+'''
+1 - Submit email form                         //PasswordResetView.as_view()
+2 - Email sent success message                //PasswordResetDoneView.as_view()
+3 - Link to password Rest form in email       //PasswordResetConfirmView.as_view()
+4 - Password successfully changed message     //PasswordResetCompleteView.as_view()
+'''
